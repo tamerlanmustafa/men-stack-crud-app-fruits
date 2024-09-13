6d3398c234d70ed0a6ff1800f9f98d4b44368352
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-09-12 20:20:22 -0400

    Make individual fruit pages

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose')
 const app = express()
 dotenv.config()
 
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+
 
 const PORT = process.env.PORT || 3000
 
@@ -28,6 +31,8 @@ const Fruit = require('./models/fruit')
 app.set('view engine', 'ejs')
 // MIDDLEWARE
 
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
 
 
 
@@ -37,12 +42,35 @@ app.use(express.urlencoded({ extended: false }));
 // ROUTES
 
 app.get("/", (req, res) => {
-    res.render('index')
+    res.render('index') 
 });
   
 
 app.get('/fruits/new',  (req, res) => {
-    res.render('fruits/new.ejs')
+    res.render('fruits/new') 
+})
+
+app.get('/fruits', async (req, res) => {
+    try {
+        const allFruits = await Fruit.find()
+        res.render('fruits/index', { fruits: allFruits })
+    } catch (err) {
+        console.log(err) 
+        res.redirect('/')
+    }
+})
+
+app.get('/fruits/:id', async (req, res) => {
+    
+    try {
+        const singleFruit = await Fruit.findById(req.params.id)
+        res.render('fruits/show', { singleFruit })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+
+
 })
 
 app.post('/fruits', async (req, res) => {
@@ -50,18 +78,17 @@ app.post('/fruits', async (req, res) => {
     if (req.body.isReadyToEat) {
         req.body.isReadyToEat = true
     } else {
-        req.body.isReadyToEat = false
+        req.body.isReadyToEat =  false
     }
     
     res.send(req.body)
     console.log(req.body)
 
     try {
-        const createFruit = await Fruit.create(req.body)
-        res.redirect('/fruits/new')
+        await Fruit.create(req.body)
+        res.redirect('/fruits/new?status=success')
     } catch (err) {
-        res.status(400).json({error: err.message})
-        console.log(err)
+        res.status(400).json({error: err.message}) 
     }
 })
 
